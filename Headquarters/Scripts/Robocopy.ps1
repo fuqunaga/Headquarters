67e2444bfcf371c1f4d1877fe25d841cd575e35d
fuqunaga <fukhid@gmail.com>
2024-04-06 15:05:59 +0900

    add Robocopy.ps1

---
+++ Headquarters/Scripts/Robocopy.ps1
@@ -0,0 +1,12 @@
+﻿param($ip, $sourceDir, $destinationDir, $file, $robocopyOptions)
+
+$src = $sourceDir.Replace("`$IP", $ip)
+$dst = $destinationDir.Replace("`$IP", $ip)
+
+$subdirOption = "/s"
+if ( [string]::IsNullOrEmpty($file) )
+{
+    $subdirOption = "/e"
+} 
+
+robocopy $src $dst $file $subdirOption /b /r:1 /compress /njh /njs /nfl /ndl /np $robocopyOptions
