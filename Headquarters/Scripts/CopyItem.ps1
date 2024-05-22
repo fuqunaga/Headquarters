421e18d307f10c11660df4ae3baa91792199b8a1
fuqunaga <fukhid@gmail.com>
2024-05-22 19:05:42 +0900

    Copy-Item use UNC

--- Headquarters/Scripts/CopyItem.ps1
+++ Headquarters/Scripts/CopyItem.ps1
@@ -1,3 +1,13 @@
 param($session,$localPath,$remotePath)
 
-Copy-Item -ToSession $session -Recurse -Force -Path $localPath -Destination $remotePath
+$remotePath = $remotePath.Replace("`$(IP)", $ip)
+$remotelPathInfo = [System.Uri]$remotePath
+
+if ($remotelPathInfo.IsUnc)
+{
+    Copy-Item -Recurse -Force -Path $localPath -Destination $remotePath
+}
+else
+{
+    Copy-Item -ToSession $session -Recurse -Force -Path $localPath -Destination $remotePath
+}
