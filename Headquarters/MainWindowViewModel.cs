433b7001fec44815ff61ff28240d02ed63fe6ebd
fuqunaga <fukhid@gmail.com>
2024-10-18 16:16:32 +0900

    MainWindow MVVM style

--- Headquarters/MainWindowViewModel.cs
+++ Headquarters/MainWindowViewModel.cs
@@ -1,8 +1,59 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Headquarters;
 
-public class MainWindowViewModel
+public class MainWindowViewModel : ViewModelBase
 {
-    public ObservableCollection<MainTabViewModel> TabItems { get; set; } = new();
+    public ObservableCollection<MainTabViewModel> TabItems { get;  } = [];
+    
+    public MainWindowViewModel()
+    {
+        LoadSettings();
+    }
+
+    public void OnClosing(object? sender, CancelEventArgs e)
+    {
+        SaveSettings();
+    }
+
+
+    private void LoadSettings()
+    {
+        var settingData = SettingManager.Instance.Load(".\\setting.json")
+                          ?? new SettingManager.SettingData()
+                          {
+                              MainTabDataList =
+                              [
+                                  new MainTabData
+                                  {
+                                      TabHeader = "Tab0",
+                                      IpList =
+                                      [
+                                          new Dictionary<string, string>()
+                                          {
+                                              { "Value1", "1" }
+                                          }
+                                      ]
+                                  }
+                              ]
+                          };
+
+        foreach (var data in settingData.MainTabDataList)
+        {
+            TabItems.Add(new MainTabViewModel(data));
+        }
+    }
+    
+    private void SaveSettings()
+    {
+        var settingData = new SettingManager.SettingData()
+        {
+            MainTabDataList = TabItems.Select(vm => vm.CreateMainTabData()).ToList()
+        };
+            
+        SettingManager.Instance.Save(".\\setting.json", settingData);
+    }
 }
