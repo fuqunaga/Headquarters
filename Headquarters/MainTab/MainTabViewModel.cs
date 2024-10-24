4368a0bf1a9c2b9a0a6ce4321b6d0159d60c4b5c
fuqunaga <fukhid@gmail.com>
2024-10-24 18:58:49 +0900

    Display Script name in tab header

--- Headquarters/MainTab/MainTabViewModel.cs
+++ Headquarters/MainTab/MainTabViewModel.cs
@@ -7,20 +7,33 @@ public class MainTabViewModel : ViewModelBase
     public static Func<MainTabViewModel> Factory => () => new MainTabViewModel();
 
     private readonly TabParameterSet _tabParameterSet;
+    private string _header;
     
     public IpListViewModel IpListViewModel { get; } = new();
     public ScriptPageViewModel ScriptPageViewModel { get; } = new();
-    
-    public string Header { get; set; }
-    
+
+    public string Header
+    {
+        get => _header;
+        set => SetProperty(ref _header, value);
+    }
 
     public MainTabViewModel(MainTabData data)
     {
-        Header = data.TabHeader;
+        _header = data.TabHeader;
         IpListViewModel.DataGridViewModel.Items = data.CreateIpListDataTable();
 
         _tabParameterSet = data.CreateTabParameterSet();
         ScriptPageViewModel.Initialize(IpListViewModel,_tabParameterSet);
+        ScriptPageViewModel.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(ScriptPageViewModel.CurrentPage))
+            {
+                Header = ScriptPageViewModel.CurrentPage == ScriptPageViewModel.Page.SelectScript
+                    ? "Select Script"
+                    : ScriptPageViewModel.CurrentScriptRunViewModel.ScriptName;
+            }
+        };
     }
 
     public MainTabViewModel() : this(new MainTabData())
---
+++ Headquarters/Utility/EnumToIntConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+#nullable disable
+
+namespace Headquarters;
+
+public class EnumToIntConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Enum)
+        {
+            return System.Convert.ToInt32(value);
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int && targetType.IsEnum)
+        {
+            return Enum.ToObject(targetType, value);
+        }
+
+        return null;
+    }
+}
