89e2ee2bc4dc1203548fbbcfd12f8d6c592fcb5a
fuqunaga <fukhid@gmail.com>
2024-10-25 18:05:04 +0900

    Save the order in which tabs are displayed

--- Headquarters/MainWindowViewModel.cs
+++ Headquarters/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace Headquarters;
 public class MainWindowViewModel : ViewModelBase
 {
     private bool _isSettingPageOpen;
+    
+    public Func<IEnumerable<MainTabViewModel>>? GetOrderedTabsFunc { get; set; }
     
     public SettingPageViewModel SettingPageViewModel { get; } = new();
     public ObservableCollection<MainTabViewModel> TabItems { get;  } = [];
@@ -56,13 +59,13 @@ public class MainWindowViewModel : ViewModelBase
         var settingData = new SettingManager.SettingData()
         {
             GlobalParameterSet = GlobalParameter.ParameterSet?.Parameters ?? new Dictionary<string, string>(),
-            MainTabDataList = TabItems.Select(vm => vm.CreateMainTabData()).ToList()
+            MainTabDataList = GetOrderedTabsFunc?.Invoke().Select(vm => vm.CreateMainTabData()).ToList() ?? []
         };
             
         SettingManager.Instance.Save(".\\setting.json", settingData);
     }
     
-    private void NewTab(MainTabViewModel sender)
+    private static void NewTab(MainTabViewModel sender)
     {
         TabablzControl.AddItem(new MainTabViewModel(), sender, AddLocationHint.After);
     }
