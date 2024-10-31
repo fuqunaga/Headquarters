61dd4b653bb1921c19be6f26a1efd6fa23cc22a5
fuqunaga <fukhid@gmail.com>
2024-10-31 17:48:47 +0900

    Script parsing errors are displayed on script buttons

--- Headquarters/ScriptPage/ScriptButtonViewModel.cs
+++ Headquarters/ScriptPage/ScriptButtonViewModel.cs
@@ -6,13 +6,15 @@ namespace Headquarters;
 
 public class ScriptButtonViewModel : ViewModelBase
 {
-    public Script Script { get; }
-    
     public string Name  => Script.Name;
     public string Synopsis => Script.Synopsis;
     public bool HasError => Script.HasError;
     
     public ICommand SelectCommand { get; }
+
+    
+    private Script Script { get; }
+
 
 
     public ScriptButtonViewModel(Script script, Action<Script>? onSelected)
