1b02d84952eaef94d32cfdb72ca716e37e3b34b5
fuqunaga <fukhid@gmail.com>
2024-10-31 17:22:26 +0900

    ScriptPage button design

--- Headquarters/PowerShellScript/Script.cs
+++ Headquarters/PowerShellScript/Script.cs
@@ -48,8 +48,8 @@ namespace Headquarters
         
         public string Name { get; } = Path.GetFileNameWithoutExtension(filepath);
         public bool HasError => _parseErrors.Length > 0;
-        public string Synopsis => _helpInfo?.Synopsis ?? "";
-        public string Description => _helpInfo?.Description ?? "";
+        public string Synopsis => _helpInfo?.Synopsis.TrimEnd('\r', '\n') ?? "";
+        public string Description => _helpInfo?.Description.TrimEnd('\r', '\n') ?? "";
         
         private ParseError[] _parseErrors = [];        
         public IEnumerable<string> EditableParameterNames => _scriptFunctionDictionary.Values.SelectMany(f => f.ParameterNames).Distinct().Except(ReservedParameterNames);
