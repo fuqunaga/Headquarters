21850579d8e63f35d34411102c2be60371a8d24a
fuqunaga <fukhid@gmail.com>
2024-11-26 16:55:26 +0900

    add Decompress-Item.ps1

---
+++ Headquarters/Scripts/Decompress-Item.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.SYNOPSIS
+ローカルPC上の圧縮ファイルを解凍します
+
+.PARAMETER ArchivePath
+解凍するファイルのパス
+
+.PARAMETER DestinationDirectory
+解凍先のディレクトリ
+存在しなければ自動的に作成します
+#>
+
+param
+(
+    $ArchivePath, 
+    $DestinationDirectory
+)
+
+try {
+    $archiveItem = Get-Item -Path $ArchivePath -ErrorAction Stop
+}
+catch {
+    throw $_.Exception.Message
+}
+
+
+# 解凍先のディレクトリが存在しない場合、作成
+if (-not (Test-Path -Path $DestinationDirectory)) {
+    New-Item -ItemType Directory -Path $DestinationDirectory > $null
+}
+
+# tarコマンドを使用して解凍
+# Write-Output "tar -xf $archiveItem -C $DestinationDirectory"
+tar -xf $archiveItem -C $DestinationDirectory
