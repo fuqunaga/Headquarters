0c4e550e2ae4fe848920db62ce0ed616263e5723
fuqunaga <fukhid@gmail.com>
2024-11-26 18:23:10 +0900

    add Start-Process.ps1

---
+++ Headquarters/Scripts/Start-Process.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+アプリケーションを実行します
+
+.PARAMETER FilePath
+開始するアプリケーションのパス
+#>
+
+param(
+    $Session, 
+    [ValidateNotNullOrEmpty()]
+    $FilePath
+)
+
+
+Invoke-Command $Session -ScriptBlock {
+    if (!(Test-Path $using:FilePath)) {
+        Write-Error "ファイル[$using:FilePath]が見つかりません。"
+        return
+    }
+
+    $taskName = "Start-Process_TempTask"
+    $action = New-ScheduledTaskAction -Execute $using:FilePath
+
+    Register-ScheduledTask -TaskName $taskName -Action $action > $null
+    Start-ScheduledTask -TaskName $taskName
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+}
