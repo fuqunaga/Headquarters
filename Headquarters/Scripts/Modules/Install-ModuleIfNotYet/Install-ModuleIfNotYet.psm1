e6741cf2f4292312d0e35a2d7de99af8200b0c93
fuqunaga <fukhid@gmail.com>
2024-12-13 20:02:40 +0900

    add InstallModuleIfNotYet module

---
+++ Headquarters/Scripts/Modules/Install-ModuleIfNotYet/Install-ModuleIfNotYet.psm1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+対象のモジュールが見つかれなければ対話しないようにInstall-Moduleを実行します
+#>
+function Install-ModuleIfNotYet {
+    param
+    (
+        [ValidateNotNullOrEmpty()]
+        $ModuleName
+    )
+
+    if (-not (Get-Module -ListAvailable $ModuleName)) {
+
+        # NuGetがインストールされていない場合はインストールする
+        Get-PackageProvider -Name "NuGet" -ForceBootstrap > $null
+
+        Write-Output "モジュールをインストールします : [$ModuleName]"
+        Install-Module -Name $ModuleName -Force -Scope CurrentUser
+    }
+}
