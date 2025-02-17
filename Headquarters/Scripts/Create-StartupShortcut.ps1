a267893a5c2719e18a598723a54f082b4b63b435
fuqunaga <fukhid@gmail.com>
2025-02-17 16:25:21 +0900

    refactor: CreateStartupShortcut.ps1

---
+++ Headquarters/Scripts/Create-StartupShortcut.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+リモートPCのスタートアップフォルダにショートカットを作成します
+
+.DESCRIPTION
+リモートPCのスタートアップフォルダにショートカットを作成します
+以下のファイルが作成されます
+C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\<$ShortcutName>.lnk
+
+.PARAMETER ShortcutName
+ショートカットファイル名
+
+.PARAMETER TargetPath
+ショートカットのリンク先
+#>
+
+param($Session, $ShortcutName, $TargetPath)
+
+Invoke-Command $Session -ScriptBlock {
+   $item = Get-Item $using:TargetPath
+   $WSH = New-Object -ComObject 'WScript.Shell'
+   $lnk = $WSH.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\$using:ShortcutName.lnk")
+   $lnk.TargetPath = $item.FullName
+   $lnk.WorkingDirectory = $item.DirectoryName
+   $lnk.Save()
+}
