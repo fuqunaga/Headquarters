b4270890915dd5b2e49eb01a8df5d905db048131
fuqunaga <fukhid@gmail.com>
2025-02-26 19:46:01 +0900

    add TaskContextUtility/Unc.psm1

--- Headquarters/Resources/Modules/TaskContextUtility/TaskContextUtility.psm1
+++ Headquarters/Resources/Modules/TaskContextUtility/TaskContextUtility.psm1
@@ -6,4 +6,5 @@ TaskContextを利用するためのユーティリティ
 # サブモジュールをインポート
 $moduleRoot = $PSScriptRoot
 Import-Module "$moduleRoot\Session.psm1" -Force
+Import-Module "$moduleRoot\Unc.psm1" -Force
 Import-Module "$moduleRoot\Pool.psm1" -Force
