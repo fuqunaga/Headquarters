ffa10238257453f86576cb0d6524c04de5b05d28
fuqunaga <fukhid@gmail.com>
2025-03-04 14:54:46 +0900

    [WIP]add TutorialScripts

---
+++ Headquarters/Resources/TutorialScripts/1-CommentBasedHelp.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+この項目は.SYNOPSISです
+スクリプトのコマンドベースのヘルプの反映のされかたを確認するためのスクリプトです
+スクリプト選択画面と、選択後のスクリプト画面の上部に表示されます
+
+.DESCRIPTION
+この項目は.DESCRIPTIONです
+スクリプト選択後の画面上部に折りたたまれた状態で表示されます
+日本語を表示する場合はBOM付きUTF-8で保存してください
+
+.PARAMETER Parameter
+この項目は.PARAMETER Parameterの１行目です
+この項目は.PARAMETER Parameterの２行目です
+１行目のみ強調表示されます
+#>
+
+
+param(
+    $Parameter
+)
+
+Write-Host "Parameter: [$Parameter]"
---
+++ Headquarters/Resources/TutorialScripts/2-SimpleScript.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+シンプルなスクリプト例です
+
+.DESCRIPTION
+スクリプトはIP Listで選択されたIPごとに並列して実行されます
+UIからパラメータの値を入力し、右上の実行ボタン（▶️）で実行するのが基本的な流れです
+
+.PARAMETER Parameter
+パラメーター
+スクリプトのパラメーターが自動的にUIに反映されます
+IP Listに同名のパラメーターがある場合、IP Listの値が優先され各IPごとに異なる値を指定できます
+#>
+
+
+param(
+    $Parameter = "This is a parameter"
+)
+
+Write-Host "Parameter: $Parameter"
